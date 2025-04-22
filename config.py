54f81ad3ada8f6a2870a6bd51a60deeb09424cf8
AKHIL <akhilesh@meridiandatalabs.com>
2025-04-22 16:31:15 +0530

    fetching all users from honeycomb

---
+++ config.py
@@ -0,0 +1,7 @@
+# base url for user fetcher from honeycomb
+BASE_URL = "http://10.12.82.105:80"
+
+# user credentials for honeycomb
+Username = "admin@mdl.com"
+Password = "grse@2024"
+
---
+++ main.py
@@ -0,0 +1,3 @@
+import logging
+
+__name__ = "__main__"
